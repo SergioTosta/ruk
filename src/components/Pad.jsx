10d526ca2ac47cf02592dd672331ebffe9fcfdc8
Sergio Tosta <sergio.tosta@gmail.com>
2018-10-03 19:34:59 -0300

    Adding images and improving layout.

--- src/components/Pad.jsx
+++ src/components/Pad.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
 import Image from "react-bootstrap/lib/Image";
 
+const imageCSS = {
+  width: "100px",
+  height: "100px"
+};
+
 class Pad extends Component {
   play(id) {
     document.getElementById(id).play();
   }
-
   render() {
     return (
-      <div onClick={() => this.play(this.props.item.key)}>
+      <div className="mt-4">
         <audio id={this.props.item.key}>
-          <source src={"data:audio/mpeg;base64," + this.props.item.data.audio} />
+          <source type="audio/mpeg" src={this.props.item.data.audio} />
         </audio>
-        <Image src={this.props.item.data.image} rounded />
+        <Image
+          src={this.props.item.data.image}
+          rounded
+          onClick={() => this.play(this.props.item.key)}
+          style={imageCSS}
+        />
         <div>{this.props.item.data.title}</div>
       </div>
     );
